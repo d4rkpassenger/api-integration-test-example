0e1d4aecf0abf20b45f096167e1e081f641c5e4e
d4rkpassenger <kishonti.david@gmail.com>
2019-04-19 19:32:55 +0200

    Add service unit test

---
+++ time.engine.api.unittests/V1/Services/TimeServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using time.engine.api.V1.Services;
+using Xunit;
+
+namespace time.engine.api.unittests.V1.Services
+{
+    public class TimeServiceTests
+    {
+
+        [Fact]
+        public void CurrentDate_Returns_ValidDate()
+        {
+            var target = new TimeService();
+
+            var result = target.CurrentDate();
+
+            Assert.NotEqual(default(DateTime), result);
+        }
+    }
+}
